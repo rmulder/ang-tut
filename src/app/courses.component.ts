bce35eede6d737bdcf6657b2702de07ec5a590c1
Christian Frauenhoffer <johncfrauen@gmail.com>
2018-11-01 15:06:15 -0500

    Learned about interpolation in templates

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses', // <div class='courses'> ".courses"
-    template: '<h2>Courses</h2>'
+    template: '<h2>{{ getTitle() }}</h2>'
 })
 export class CoursesComponent {
+    title = "List of courses";
+
+    getTitle() {
+        return this.title;
+    }
 }
+
