013965048ceac222758a9b452ea116ea9e628047
Christian Frauenhoffer <johncfrauen@gmail.com>
2018-11-01 17:08:42 -0500

    Added Class binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,23 +1,13 @@
-import { CoursesService } from './courses.service';
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'courses', // <div class='courses'> ".courses"
+    selector: 'courses',
     template: `
-        <h2>{{ getTitle() }}</h2>
-        <ul>
-            <li *ngFor="let course of courses">
-                {{ course }}
-            </li>
-        </ul>
+        <button class="btn btn-primary" [class.active]="isActive">Save</button>
     `
 })
 export class CoursesComponent {
-    title = "List of courses";
-    courses;
-
-    constructor(service: CoursesService) {
-        this.courses = service.getCourses();
-    }
+    isActive = false;
 }
+
 
