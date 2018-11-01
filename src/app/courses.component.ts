98c3f638feeca7e2fc2d302358f3d9b93e829655
Christian Frauenhoffer <johncfrauen@gmail.com>
2018-11-01 14:48:31 -0500

    Learned how to add components

---
+++ src/app/courses.component.ts
@@ -0,0 +1,9 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'courses', // <div class='courses'> ".courses"
+    template: '<h2>Courses</h2>'
+})
+export class CoursesComponent {
+}
