8e73c7f4f3fffd7ffb7f59f03bb7228cdc0f25ac
Christian Frauenhoffer <johncfrauen@gmail.com>
2018-11-08 09:46:01 -0600

    Added drop down lists to form

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent {
+  contactMethods = [
+    { id: 1, name: 'Email' },
+    { id: 2, name: 'Phone' }
+  ];
   log(x) { console.log(x); }
+
+  submit(f) {
+    console.log(f);
+  }
 }
