4fc34dcd376365fe570884462282ceb1584240dc
Christian Frauenhoffer <johncfrauen@gmail.com>
2018-11-08 14:55:48 -0600

    Added async validators

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -10,11 +10,9 @@ import { UsernameValidators } from './username.validators';
 export class SignupFormComponent {
 
   form = new FormGroup({
-    username: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      UsernameValidators.cannotContainSpace
-    ]),
+    username: new FormControl('', 
+      Validators.required, 
+      UsernameValidators.shouldBeUnique),
     password: new FormControl('', Validators.required)
   });
 
